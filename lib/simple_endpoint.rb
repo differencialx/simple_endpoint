d92f94e5c3a98600fa34d4a5fcafc526d2e69a95
Alex Bal <differencialx@gmail.com>
2019-03-03 01:52:59 +0200

    Add main logic and basic tests

--- lib/simple_endpoint.rb
+++ lib/simple_endpoint.rb
@@ -1,5 +1,92 @@
 require "simple_endpoint/version"
 
 module SimpleEndpoint
-  # Your code goes here...
+  class OperationIsNotHandled < StandardError ;end
+  class UnhadledResultError < StandardError ;end
+
+  module Controller
+    def endpoint(operation:, different_cases: {}, options: endpoint_options, &block)
+      Endpoint.(
+        operation,
+        default_handler,
+        default_cases.merge(different_cases),
+        options,
+        &block
+      )
+    end
+
+    protected
+    
+    def endpoint_options(**additional_options)
+      { params: params }.merge(additional_options)
+    end
+
+    def default_handler
+      raise NotImplementedError, <<-LARGE_ERROR
+        Please implement handler via case statement
+
+        EXAMPLE:
+        ###############################################
+
+        -> (kase, result) do
+          case kase
+          when :success then render json
+          else
+            # just in case you forgot to add handler for some of case
+            SimpleEndpoint::UnhadledResultError, 'Oh nooooo!!! Really???!!'
+          end
+        end
+
+        ###############################################
+
+        OR
+
+        You can move this logic to separate singleton class
+      LARGE_ERROR
+    end
+
+    def default_cases
+      raise NotImplementedError, <<-LARGE_ERROR
+        Please implement default cases conditions via hash
+
+        EXAMPLE:
+        ###############################################
+        # default trailblazer-endpoint logic, you can change it 
+        {
+          present:         -> (result) { result.success? && result["present"] }
+          success:         -> (result) { result.success? },
+          created:         -> (result) { result.success? && result["model.action"] == :new }
+          invalid:         -> (result) { result.failure? },
+          not_found:       -> (result) { result.failure? && result["result.model"] && result["result.model"].failure? },
+          unauthenticated: -> (result) { result.failure? && result["result.policy.default"] && result["result.policy.default"].failure? }
+        }
+
+        ###############################################
+
+        OR
+
+        You can move this to separate singleton class
+      LARGE_ERROR
+    end
+  end
+
+  class Endpoint
+    def self.call(operation, handler, cases, **args, &block)
+      result = operation.(**args)
+      new.(result, cases, handler, &block)
+    end
+
+    def call(result, cases, handler=nil, &block)
+      matcher.(result, cases, &block) if block_given?
+      matcher.(result, cases, &handler)
+    end
+
+    def matcher
+      -> (result, cases, &block) {
+        matched_kase = cases.select { |kase, cond| cond.(result) }.to_a.flatten.first # can be optimized
+        raise OperationIsNotHandled, 'Come on guys!!!' unless matched_kase
+        block.(matched_kase, result)
+      }
+    end
+  end
 end
--- spec/simple_endpoint_spec.rb
+++ spec/simple_endpoint_spec.rb
@@ -1,9 +1,110 @@
 RSpec.describe SimpleEndpoint do
-  it "has a version number" do
-    expect(SimpleEndpoint::VERSION).not_to be nil
+  Dummy = Class.new do
+    include SimpleEndpoint::Controller
+
+    attr_accessor :instance_context, :before_context
+
+    def initialize
+      @instance_context = nil
+      @before_context = nil
+    end
+
+    def default_handler
+      -> (kase, result) {
+        case kase
+        when :success then @instance_context = result.success
+        when :invalid then @instance_context = result.failure
+        else
+          raise SimpleEndpoint::UnhadledResultError, 'Oh nooooo!!! Really???!!'
+        end
+      }
+    end
+
+    def default_cases
+      {
+        success: -> (result) { result.success? },
+        invalid: -> (result) { result.failure? }
+      }
+    end
+
+    def params
+      { controller_param: 'controller_param' }
+    end
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  let(:operation_class) { double('Operation class', call: result) }
+  let(:result) { double('Trailbalzer operation result', success: 'Success', failure: 'Failure') }
+
+  let(:args) { { operation: operation_class } }
+  let(:dummy_instance) { Dummy.new }
+
+  subject(:endpoint) { dummy_instance.endpoint(**args) }
+
+  context 'success case' do
+    before do
+      expect(result).to receive(:success?).at_most(:twice) { true }
+      expect(result).to receive(:failure?).at_most(:twice) { false }
+    end
+
+    context 'default behavior' do
+      it do
+        endpoint
+        expect(dummy_instance.before_context).to be_nil
+        expect(dummy_instance.instance_context).to eq 'Success'
+      end
+    end
+
+    context 'redefined cases' do
+      let(:different_cases) do
+        {
+          success: -> (result) { result.failure? },
+          invalid: -> (result) { result.success? }
+        }
+      end
+      let(:args) do
+        {
+          operation: operation_class,
+          different_cases: different_cases
+        }
+      end
+
+      it do
+        endpoint
+        expect(dummy_instance.before_context).to be_nil
+        expect(dummy_instance.instance_context).to eq 'Failure'
+      end
+    end
+
+    context 'before handler action' do
+      let(:args) do
+        {
+          operation: operation_class,
+          options: dummy_instance.send(:endpoint_options, some_key: 'some value')
+        }
+      end
+
+      it do
+        expect(operation_class).to receive(:call).with(
+          {
+            params: { controller_param: 'controller_param' },
+            some_key: 'some value'
+          }
+        )
+        endpoint
+      end
+    end
+
+    context 'before handler actions' do
+      it do
+        dummy_instance.endpoint(**args) do |kase, result|
+          case kase
+          when :success 
+            dummy_instance.before_context = result.success
+          end
+        end
+        expect(dummy_instance.before_context).to eq 'Success'
+        expect(dummy_instance.instance_context).to eq 'Success'
+      end
+    end
   end
 end
